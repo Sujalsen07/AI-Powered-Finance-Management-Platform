48d6d6816d9ef25e97332ad0eca9a6ace1b4fd97
Sujal Sen <ssen25255@gmail.com>
2026-01-13 22:57:53 +0530

    filtering

--- app/(main)/account/_components/transaction-table.jsx
+++ app/(main)/account/_components/transaction-table.jsx
@@ -68,7 +68,60 @@ const TransactionTable = ({ transactions }) => {
   const [typeFilter, setTypeFilter] = useState("");
   const [recurringFilter, setRecurringFilter] = useState("");
 
-  const filteredAndSortedTransactions = transactions; // Placeholder for actual filtering and sorting logic
+  const filteredAndSortedTransactions = useMemo(() => {
+    let result = [...transactions];
+
+    //Apply search filter
+    if(searchTerm) {
+      const searchLower = searchTerm.toLowerCase();
+      result = result.filter((transaction)=>
+        transaction.description?.toLowerCase().includes(searchLower)
+      );
+    }
+
+    //Apply recurring filter
+    if(recurringFilter){
+      result  = result.filter((transaction)=>{
+        if(recurringFilter === "recurring") return transaction.isRecurring;
+        return !transaction.isRecurring;
+      });
+    }
+
+    //Apply type filter
+    if(typeFilter){
+      result = result.filter((transaction)=> transaction.type === typeFilter);
+    }
+
+    //Apply Sorting
+    result.sort((a,b)=>{
+      let comparison = 0
+
+      switch (sortConfig.field) {
+        case "date":
+            comparison = new Date(a.date) - new Date(b.date);
+          break;
+        case "amount":
+          comparison = a.amount - b.amount;
+          break;
+        case "category":
+          comparison = a.category.localCompare(b.category);
+          break; 
+
+        default:
+          comparison = 0;
+      }
+
+      return sortConfig.direction === "asc" ? comparison : -comparison;
+    })
+
+    return result;
+  }, [
+    transactions,
+    searchTerm,
+    typeFilter,
+    recurringFilter,
+    sortConfig,
+  ]); // Placeholder for actual filtering and sorting logic
   
   const handleSort = (field) => {
     setSortConfig((current) => ({
