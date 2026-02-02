7e81fcdc0e2ed183cf85ff4b52b0b1570fafc04d
Sujal Sen <ssen25255@gmail.com>
2026-02-02 23:20:35 +0530

    email update

--- emails/template.jsx
+++ emails/template.jsx
@@ -1,7 +1,15 @@
 import { Button, Html, Head, Body } from "@react-email/components";
 import * as React from "react";
 
-export default function Email() {
+export default function Email({
+  userName= "",
+  type="budget-alert",
+  data = {},
+}) {
+  if(type === "monthly-report"){
+  }
+  if(type === "budget-alert"){
+  }
   return (
     <Html>
       <Head />
